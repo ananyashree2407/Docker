bb1191154a0f08853df659c59467a94dd247de64
Ananya <ananyashree240701@gmail.com>
2023-09-20 11:19:45 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+def hello():
+    return "Hii this is a sample repository for CI-CD pipeline"
+
+def main():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
+
+if __name__ == "__hello__":
+    hello()
+    
